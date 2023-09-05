b6ec01f265f6b697d941352872aeee5f23351442
Facundo Herrera <facundhfed@gmail.com>
2023-09-05 14:11:13 +0200

    request-services finished

--- src/api/request.service.ts
+++ src/api/request.service.ts
@@ -1,20 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 
-export const postUser = async (data: any, getToken: any): Promise<void> => {
-  const { VITE_API_URL: url } = import.meta.env;
-  const token = await getToken();
-
-  await fetch(`${url}/users`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(data),
-  });
-};
-
 export const getGenres = async (url: string) => {
   try {
     const genres = await fetch(url);
